f130ea0a1ba546bd44e6f9bc69d054979b5dfe95
Bruno Croci <crocidb@gmail.com>
2022-12-01 13:45:28 +0100

    fixing identation again

--- 1/elf.rkt
+++ 1/elf.rkt
@@ -2,15 +2,13 @@
 
 (define (count-elf total maximum)
    (define str (read-line (current-input-port) 'any))
-      (cond
-         [(string? str)
+   (cond
+      [(string? str)
          (define b (string->number str))
          (cond
             [b (count-elf (+ b total) maximum)]
             [else
-               (if (= 0 total)
-					   maximum
-                  (count-elf 0 (if (> total maximum) total maximum)))])]
-		   [else maximum]))
+               (if (= 0 total) maximum (count-elf 0 (if (> total maximum) total maximum)))])]
+      [else maximum]))
 
 (count-elf 0 0)
