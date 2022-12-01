c28890d36d2991fdd092c833c96f4b7111b083ed
Bruno Croci <crocidb@gmail.com>
2022-12-01 12:34:35 +0000

    first solution

---
+++ 1/elf.rkt
@@ -0,0 +1,17 @@
+#lang racket
+
+(define (count-elf total maximum)
+   (define str (read-line (current-input-port) 'any))
+     (cond
+          [(string? str)
+	       (define b (string->number str))
+	            (cond
+		            [b
+			            (count-elf (+ b total) maximum)]
+				           [else
+					           (if (= 0 total)
+						                maximum
+								            (count-elf 0 (if (> total maximum) total maximum)))])]
+		        [else maximum]))
+
+(count-elf 0 0)
