10010406a003ac2cf38d94bafa90d7e8f9a49e4f
Bruno Croci <crocidb@gmail.com>
2022-12-02 19:10:43 +0100

    fixing identation in 2/2

--- 2/rock-paper-scissors2.rkt
+++ 2/rock-paper-scissors2.rkt
@@ -7,19 +7,21 @@
   (define-values (a b) (let ([l (map string (string->list str))]) (values (car l) (car (cdr (cdr l))))))
   (rock-paper-scissors (+ total (score2 a b)))]
   [else total]))
-  ; first exercise
+  
+; first exercise
 (define (score1 a b)
  (+ (list-ref (rotate '(3 0 6) (letter-index b)) (letter-index a)) (list-ref '(1 2 3) (letter-index b))))
 
-  ; seconds exercise
-  (define (score2 a b)
-   (let ([c (pick-hand a b)])
-    (+ (list-ref (rotate '(3 0 6) (letter-index c)) (letter-index a)) (list-ref '(1 2 3) (letter-index c)))))
+; seconds exercise
+(define (score2 a b)
+  (let ([c (pick-hand a b)])
+  (+ (list-ref (rotate '(3 0 6) (letter-index c)) (letter-index a)) (list-ref '(1 2 3) (letter-index c)))))
 
 (define (pick-hand a b)
  (list-ref (rotate '("C" "A" "B") (- 3 (letter-index b))) (letter-index a)))
 
-  (define (rotate l n) (let ([nn (- 3 n)]) (append (drop l nn) (take l nn))))
+(define (rotate l n) (let ([nn (- 3 n)]) (append (drop l nn) (take l nn))))
+
 (define (letter-index x)
  (cond
   [(or (string=? x "A") (string=? x "X")) 0]
@@ -27,4 +29,3 @@
   [else 2]))
 
 (rock-paper-scissors 0)
-
