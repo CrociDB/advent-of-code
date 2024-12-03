561b073d18a47a3f65a017e83abb98502ec8ca5d
Bruno Croci <crocidb@gmail.com>
2024-12-03 14:40:16 +0000

    formatting code

--- 2024/3/3-2.hs
+++ 2024/3/3-2.hs
@@ -1,4 +1,4 @@
-import Text.Regex.TDFA ((=~), getAllTextMatches)
+import Text.Regex.TDFA (getAllTextMatches, (=~))
 
 parseMulInput :: String -> [String]
 parseMulInput input = getAllTextMatches (input =~ "(mul\\([0-9]+,[0-9]+\\))|(do\\(\\))|(don\'t\\(\\))")
@@ -6,14 +6,14 @@ parseMulInput input = getAllTextMatches (input =~ "(mul\\([0-9]+,[0-9]+\\))|(do\
 parseNumbers :: String -> [Int]
 parseNumbers input = map read $ getAllTextMatches (input =~ "[0-9]+")
 
-getAllEnabled [] instr enabled = instr
-getAllEnabled (x:xs) instr enabled = case x of
-  "do()" -> getAllEnabled xs instr True
-  "don't()" -> getAllEnabled xs instr False
-  _ -> getAllEnabled xs (if enabled then instr ++ [x] else instr) enabled
+getEnabled [] instr enabled = instr
+getEnabled (x : xs) instr enabled = case x of
+    "do()"    -> getAllEnabled xs instr True
+    "don't()" -> getAllEnabled xs instr False
+    _         -> getAllEnabled xs (if enabled then instr ++ [x] else instr) enabled
 
 main = do
-  input <- getContents
-  let m = parseMulInput input
-  let s = getAllEnabled m [] True
-  print $ sum $ map (product . parseNumbers) s
+    input <- getContents
+    let m = parseMulInput input
+    let s = getEnabled m [] True
+    print $ sum $ map (product . parseNumbers) s
